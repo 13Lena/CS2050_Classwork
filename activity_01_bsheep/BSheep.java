b54e20bbf6edf0cfbdab162118b8f5a8f17afb5a
Lena Hamilton <hamiltonlena21@gmail.com>
2021-06-02 12:29:23 -0600

    all test text files added, begining of java work

---
+++ activity_01_bsheep/BSheep.java
@@ -0,0 +1,20 @@
+/*
+ * CS2050 - Computer Science II - Summer 2021
+ * Instructor: Thyago Mota
+ * Description: Activity 01 - BSheep
+ */
+import java.util.Scanner;
+
+public class BSheep {
+
+    static final int MIN_NUM_SHEEP = 2;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("number of sheep?");
+        int nSheep = sc.nextInt();
+        
+        System.out.println("number of sheep: " + nSheep);
+
+    }
+} // end of class definition
